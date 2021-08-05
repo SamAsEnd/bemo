ebd45c261969c6a805aa7cffe950b69c70c0a5f6
Samson Endale <4sam21@gmail.com>
2021-08-05 12:41:56 +0300

    Remove bootstrap, jquery and lodash

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,13 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.axios = require('axios');
 
-require('./bootstrap');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Vue = require('vue').default;
 
-let VModal = require('vue-js-modal').default;
-window.Vue.use(VModal);
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('columns', require('./components/ColumnsComponent.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+let VModal = require('vue-js-modal').default;
+Vue.use(VModal);
 
 const app = new Vue({
     el: '#app',
